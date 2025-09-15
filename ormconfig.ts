c27c53bb536e8c669ba80994955de6baded78543
innovaforge-admin <info@getinnovaforge.com>
2025-09-15 19:24:58 -0300

    Add files via upload

---
+++ ormconfig.ts
@@ -0,0 +1,19 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const config: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'stock_sale',
+  synchronize: false,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+};
+
+const AppDataSource = new DataSource(config);
+
+export default AppDataSource;
