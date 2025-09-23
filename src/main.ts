b6261241a7d2cd34dd577f31923693642c967446
Pablo Gennoni <p.gennoni@gmail.com>
2025-09-23 16:47:14 -0300

    added endpoint

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+ // Habilitar CORS
+ app.enableCors({
+  origin: 'http://localhost:9002', // o un array de orígenes
+  credentials: true, // si usás cookies o headers de autenticación
+});
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
